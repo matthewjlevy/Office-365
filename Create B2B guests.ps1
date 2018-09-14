dc61ad7bd0dbf1f8dc1e970c08e2e11ce5f1baa5
Matthew Levy <matthewjlevy@gmail.com>
2018-09-14 15:53:19 +0200

    Add files via upload

---
+++ Create B2B guests.ps1
@@ -0,0 +1,17 @@
+﻿$cred = Get-Credential
+Connect-AzureAD -Credential $cred
+
+$invitations = import-csv 'C:\Users\Matt\OneDrive - NBConsult\Documents\Azure AD\LavalampB2Bguests.csv'
+$messageInfo = New-Object Microsoft.Open.MSGraph.Model.InvitedUserMessageInfo
+$messageInfo.customizedMessageBody = "Hey there! Check this out. I created an invitation through PowerShell"
+
+$email.name = "MatthewJLevy"
+$email.InvitedUserEmailAddress = 'matt.hew.jlevy@gmail.com'
+
+foreach ($email in $invitations) {
+Write-Host $email.name, $email.invitedUserEmailAddress
+}
+{New-AzureADMSInvitation -InvitedUserEmailAddress $email.InvitedUserEmailAddress -InvitedUserDisplayName $email.Name -InviteRedirectUrl https://wingtiptoysonline-dev-ed.my.salesforce.com -InvitedUserMessageInfo $messageInfo -SendInvitationMessage $true}
+
+
+New-AzureADMSInvitation -InvitedUserEmailAddress $email.InvitedUserEmailAddress -SendInvitationMessage $true -InviteRedirectUrl https://nbconsulting.sharepoint.com -InvitedUserMessageInfo $messageInfo
---
+++ Enable-MFA.ps1
@@ -0,0 +1,20 @@
+﻿$Credential = (Get-Credential -Message "Supply credentials for O365")
+Connect-MsolService -Credential $Credential
+
+#Complete the full path to your exceptions file below
+$exceptions = Import-Csv 'C:\users\Matt\OneDrive - NBConsult\Documents\Powershell Scripts\Office 365\MFAExceptions.csv'
+
+$auth = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+
+$auth.RelyingParty = "*"
+$auth.State = "Enforced"
+$auth.RememberDevicesNotIssuedBefore = (Get-Date)
+
+$users = Get-MsolUser -All | select UserPrincipalName,StrongAuthenticationRequirements
+
+$usernoMFA = $users | Where-Object {$_.StrongAuthenticationRequirements.state -ne "Enforced"}
+$usernoMFA = $usernoMFA |Where-Object {$exceptions.Userprincipalname -notcontains $_.userprincipalname}
+
+Foreach ($user in $usernoMFA)
+{Set-MsolUser -UserPrincipalName $user.UserPrincipalName -StrongAuthenticationRequirements $auth}
+
